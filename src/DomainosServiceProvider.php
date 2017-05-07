42b597ef97ecc16523fcb0be20790b50a24bd62e
John Olawale <mojoblanco@gmail.com>
2017-05-07 11:47:53 +0100

    Initial commit

---
+++ src/DomainosServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mojoblanco\Domainos;
+
+use Illuminate\Support\ServiceProvider;
+
+class DomainosServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
