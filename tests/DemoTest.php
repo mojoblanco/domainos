ab942e4d45d3175fb351dbfdf8bdf2b7e2ddcf86
John Olawale <mojoblanco@gmail.com>
2017-09-24 10:24:49 +0100

    added tests

---
+++ tests/DemoTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Tests\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function testSomethingIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testSomethingElseIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
